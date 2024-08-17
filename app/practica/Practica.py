6b3b28a3862557418378716f87cbeb10dcbbc149
emmanuelnavas31 <emmanuelnavas31@outlook.com>
2024-08-17 18:07:58 -0500

    Seccion de practica, variables y metodos

--- app/practica/Practica.py
+++ app/practica/Practica.py
@@ -1,12 +1,32 @@
-
-
-def main_practica():
-    teoria = """
-            '1': "Que es la semantica"
-            '2': "Sintaxis en python"
-            """
-    tipo_teoria = input(int(f"Que tipo de teoria quiere ver? \n {teoria}\n >> "))
+def metodos_variables():
+    print("""
+    .count() 
+    .capitalize()
+    .title() 
+    .swapcase() 
+    .replace(,) 
+    .split() 
+    .strip() 
+    .lstrip() 
+    .rstrip() 
+    .find()
+    .index() 
+    eval()	#Este y el siguiente son super métodos
+    exec()
+    """)
 
 
 def tipo_variables():
     print("Tipo de variable")
+
+
+def main_practica():
+    teoria = """
+            '1': "Tipo de variable"
+            '2': "Metodos de variable"
+            """
+    tipo_teoria = input(f"Que tipo de teoria quiere ver? \n {teoria}\n >> ")
+    if tipo_teoria == "1":
+        tipo_variables()
+    if tipo_teoria == "2":
+        metodos_variables()
